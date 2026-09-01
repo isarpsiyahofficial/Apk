0af2ccb9c999245a688bfccde65c4647e3004b5a
isarpsiyahofficial <isarpsiyahofficial@gmail.com>
2026-09-01 07:00:46 +0300

    feat(share): apply bounded text preferences to religious cards

--- lib/features/share/presentation/runtime_religious_share_card_t0243.dart
+++ lib/features/share/presentation/runtime_religious_share_card_t0243.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/widgets.dart';
 import 'package:islami_hayat/features/share/domain/runtime_religious_content_t0243.dart';
 import 'package:islami_hayat/features/share/domain/share_canvas_layout_t0242.dart';
+import 'package:islami_hayat/features/share/domain/share_text_preferences_t0246.dart';
 import 'package:islami_hayat/features/share/presentation/share_layout_renderer_t0242.dart';
 import 'package:islami_hayat/l10n/app_localizations.dart';
 
@@ -9,12 +10,14 @@ class RuntimeReligiousShareCardT0243 extends StatelessWidget {
     required this.format,
     required this.background,
     required this.content,
+    this.textPreferences = const ShareTextPreferencesT0246(),
     super.key,
   });
 
   final ShareCanvasFormatT0242 format;
   final Widget background;
   final RuntimeReligiousShareContentT0243 content;
+  final ShareTextPreferencesT0246 textPreferences;
 
   @override
   Widget build(BuildContext context) {
@@ -38,12 +41,14 @@ class RuntimeReligiousShareCardT0243 extends StatelessWidget {
           ],
           Text(
             content.text,
-            textAlign: TextAlign.center,
+            key: const ValueKey('t0246-immutable-religious-text'),
+            textAlign: textPreferences.textAlign,
+            style: TextStyle(fontSize: textPreferences.fontSizeFor(24)),
           ),
           const SizedBox(height: 16),
           Text(
             content.sourceLabel,
-            textAlign: TextAlign.center,
+            textAlign: textPreferences.textAlign,
           ),
         ],
       ),
