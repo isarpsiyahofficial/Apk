505127c79c447f4f15070bde90b4ac6ffa86a78d
isarpsiyahofficial <isarpsiyahofficial@gmail.com>
2026-09-09 07:06:17 +0300

    fix: import Flutter Locale for T0262 app integration test

--- test/app_startup_access_t0262_test.dart
+++ test/app_startup_access_t0262_test.dart
@@ -1,3 +1,4 @@
+import 'package:flutter/widgets.dart';
 import 'package:flutter_test/flutter_test.dart';
 import 'package:islami_hayat/app.dart';
 import 'package:islami_hayat/core/network/internet_reachability.dart';
