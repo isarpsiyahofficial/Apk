24b9dbb4d8f4168104010e710ac878e37d159630
isarpsiyahofficial <isarpsiyahofficial@gmail.com>
2026-09-14 00:01:37 +0300

    T0336 include batch35 in unresolved manifest source

--- lib/features/prophets/data/prophet_semantic_unresolved_t0336.dart
+++ lib/features/prophets/data/prophet_semantic_unresolved_t0336.dart
@@ -2,6 +2,7 @@ import 'canonical_prophets.dart';
 import 'prophet_hadith_evidence_t0336_batch32.dart';
 import 'prophet_hadith_evidence_t0336_batch33.dart';
 import 'prophet_hadith_evidence_t0336_batch34.dart';
+import 'prophet_hadith_evidence_t0336_batch35.dart';
 import 'prophet_semantic_evidence_t0336.dart';
 import 'prophet_semantic_ownership_qa.dart';
 
@@ -23,6 +24,7 @@ List<ProphetSemanticClaim> buildProphetSemanticEvidenceWithUnresolvedT0336({
       ...prophetHadithEvidenceT0336Batch32,
       ...prophetHadithEvidenceT0336Batch33,
       ...prophetHadithEvidenceT0336Batch34,
+      ...prophetHadithEvidenceT0336Batch35,
     ],
   );
   final output = <ProphetSemanticClaim>[...base];
