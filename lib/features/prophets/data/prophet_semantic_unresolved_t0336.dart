6500262ce6c31be20cd815e3ca75fa88daf8c879
isarpsiyahofficial <isarpsiyahofficial@gmail.com>
2026-09-13 23:01:46 +0300

    feat(prophets): wire batch 34 into T0336 manifest

--- lib/features/prophets/data/prophet_semantic_unresolved_t0336.dart
+++ lib/features/prophets/data/prophet_semantic_unresolved_t0336.dart
@@ -1,6 +1,7 @@
 import 'canonical_prophets.dart';
 import 'prophet_hadith_evidence_t0336_batch32.dart';
 import 'prophet_hadith_evidence_t0336_batch33.dart';
+import 'prophet_hadith_evidence_t0336_batch34.dart';
 import 'prophet_semantic_evidence_t0336.dart';
 import 'prophet_semantic_ownership_qa.dart';
 
@@ -21,6 +22,7 @@ List<ProphetSemanticClaim> buildProphetSemanticEvidenceWithUnresolvedT0336({
       ...canonicalProphetSemanticEvidenceT0336,
       ...prophetHadithEvidenceT0336Batch32,
       ...prophetHadithEvidenceT0336Batch33,
+      ...prophetHadithEvidenceT0336Batch34,
     ],
   );
   final output = <ProphetSemanticClaim>[...base];
